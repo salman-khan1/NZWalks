d87e53489dcb76f5a93e47877d132f4b1f3bf7e0
lenovo <lenovo@DESKTOP-GQLLVGD>
2023-07-12 19:25:23 +0500

    Add project files.

---
+++ NZWalksAPi/Controllers/StudentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPi.Controllers
+
+{   //https://locahost:portnumber/api/students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET:https://locahost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] StudentNames = new string[] {"John","jane","dev","david","Mike","Elon"};
+            return Ok(StudentNames);
+        }
+    }
+}
---
+++ NZWalksAPi/Controllers/TeachersController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalksAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeachersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllTeachers()
+        {
+            string[] Teachers= new string[] {"Kerry","Mike","Luke","Tom"};
+            return Ok(Teachers);
+        }
+    }
+}
